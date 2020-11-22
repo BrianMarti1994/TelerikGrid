633e91171da735a8129cecf232495427b8471faf
Brian <brianmarti1994@gmail.com>
2020-11-22 18:33:07 +0300

    init

---
+++ TelerikGrid/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelerikGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Create List Variable 
+
+            List<Student> list = new List<Student>();
+
+            //Initialize  Entity
+
+            using (SchoolEntities dc = new SchoolEntities())
+            {
+                list = dc.Students.OrderBy(a => a.StudentID).ToList();
+            }
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TelerikGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TelerikGrid.Startup))]
+namespace TelerikGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
